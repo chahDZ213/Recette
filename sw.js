f6d98fa133ddfd5e5118454f4c2dcb3204413803
chahDZ213 <chahchurin@gmail.com>
2026-07-06 19:51:20 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js — service worker minimal (network-first)
-const CACHE = "mise-v41";
+const CACHE = "mise-v42";
 
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(["/", "/index.html"])).catch(() => {}));
